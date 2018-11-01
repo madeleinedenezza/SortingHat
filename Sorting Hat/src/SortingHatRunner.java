20ce3454f2b8f026cf5079ce57b1afff60af2f49
madeleine.denezza <madeleine.denezza@MU-C1-10.mullen.pvt.k12.co.us>
2018-11-01 09:04:19 -0600

    add edit main method

--- Sorting Hat/src/SortingHatRunner.java
+++ Sorting Hat/src/SortingHatRunner.java
@@ -7,10 +7,15 @@ public class SortingHatRunner extends JFrame
 	private static final long serialVersionUID = 1L;
 	static JFrame frame = new JFrame();
 	static JFrame frame1 = new JFrame();
+	static JFrame frame2 = new JFrame();
+	static JFrame frame3 = new JFrame();	
+	static JFrame frame4 = new JFrame();
 	static int learnMore;
 	static String sortingHat;
 	static String firstPeriod;
-	
+	static String secondPeriod;
+	static String thirdPeriod;
+	static String fourthPeriod;
 	static String name; 
 	
 	public static void main(String[] args)
@@ -97,27 +102,120 @@ public class SortingHatRunner extends JFrame
 		
 		JOptionPane.showMessageDialog(frame, "Time to start your classes! You'll have 5 classes to go to today.");
 		
-		String[] periodOne = {"Transfiguratons", "Charms"};
+		String[] periodOne = {"Transfigurations", "Charms"};
 		JFrame frame1 = new JFrame();
 		String firstPeriod = (String) JOptionPane.showInputDialog(frame1, "Which class would you like to take during first period?",
 				"First Period",
 				JOptionPane.QUESTION_MESSAGE,
-				null
+				null,
 				periodOne,
 				periodOne[0]);
-		break;
 		
 		switch(firstPeriod)
 			{
-			case "Transfiguartions":
+			case "Transfigurations":
 				{
 				ImageIcon icon1 = new ImageIcon(("transfigurations.jpg"));
 				JOptionPane.showMessageDialog(frame, "Great choice! Transfiguartions is a great class! Your professor, Professor McGonagall, is a great teacher!",
 						"Transfigurations", 0, icon1);
+				break;
+				}
+			case "Charms":
+				{
+				ImageIcon icon1 = new ImageIcon(("charms.jpg"));
+				JOptionPane.showMessageDialog(frame, "Great choice! Charms is a great class! "
+						+ "\nYour professor, Professor Flitwick, is one of the best teachers here at Hogwarts! He is also the head of the greatest Hogwarts house, Ravenclaw!",
+						"Charms", 0, icon1);
+				break;
 				}
 			}
 		
-
+		JOptionPane.showMessageDialog(frame, "Let's move onto second period!");
+		
+		String[] periodTwo = {"Potions", "History of Magic"};
+		JFrame frame2 = new JFrame();
+		String secondPeriod = (String) JOptionPane.showInputDialog(frame2, "Which class would you like to take during second period?",
+				"Second Period",
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				periodTwo,
+				periodTwo[0]);
+		
+		switch(secondPeriod)
+			{
+			case "Potions":
+				{
+				ImageIcon icon1 = new ImageIcon(("potions.jpg"));
+				JOptionPane.showMessageDialog(frame, "Great choice! Potions is a wonderful class. You will learn so much from the great Professor Snape!",
+						"Potions", 0, icon1);
+				break;
+				}
+			case "History of Magic":
+				{
+				ImageIcon icon1 = new ImageIcon(("historyOfMagic.jpg"));
+				JOptionPane.showMessageDialog(frame, "Wonderful choice. Professor Binns is full of knowledge on the history of magic!",
+						"History of Magic", 0, icon1);
+				break;
+				}
+			}
+				
+		JOptionPane.showMessageDialog(frame, "Time to move onto third period!");
+		
+		String[] periodThree = {"Defence Against the Dark Arts", "Astronomy"};
+		JFrame frame3 = new JFrame();
+		String thirdPeriod = (String) JOptionPane.showInputDialog(frame3, "Which class would you like to take during third period?",
+				"Third Period",
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				periodThree,
+				periodThree[0]);
+		
+		switch(thirdPeriod)
+			{
+			case "Defence Against the Dark Arts":
+				{
+				ImageIcon icon1 = new ImageIcon(("dATDA.jpg"));
+				JOptionPane.showMessageDialog(frame, "Very good choice! Defence Against the Dark Arts is a very challenging, but rewarding class!"
+						+ "\nProfessor Snape is a very good teacher!",
+						"Defence Against the Dark Arts", 0, icon1);
+				break;
+				}
+			case "Astronomy":
+				{
+				ImageIcon icon1 = new ImageIcon(("astronomy.jpg"));
+				JOptionPane.showMessageDialog(frame, "Very good choice! Astronomy with Professor Sinistra is a great class!",
+						"Astronomy", 0, icon1);
+				break;
+				}
+			}
+		
+		JOptionPane.showMessageDialog(frame, "Let's go to fourth period now!");
+		
+		String[] periodFour = {"Herbology", "Flying"};
+		JFrame frame4 = new JFrame();
+		String fourthPeriod = (String) JOptionPane.showInputDialog(frame4, "Which class would you like to take during fourth period?",
+				"Fourth Period",
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				periodFour,
+				periodFour[0]);
+		
+		switch(fourthPeriod)
+			{
+			case "Herbology":
+				{
+				ImageIcon icon1 = new ImageIcon(("herbology.jpg"));
+				JOptionPane.showMessageDialog(frame, "Herbology is a great class! Professor Sprout is just wonderful!",
+						"Herbology", 0, icon1);
+				break;
+				}
+			case "Flying":
+				{
+				ImageIcon icon1 = new ImageIcon(("flying.jpg"));
+				JOptionPane.showMessageDialog(frame, "Flying with Professor Hooch is a great class, especially if you plan on joining the Quidditch team!",
+						"Flying", 0, icon1);
+				break;
+				}
+			}
 		}
-
 	}
